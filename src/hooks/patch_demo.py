e376b61d63b82db054a0a078d0867ee98ad9f04a
Nikhil Nagaraj <nikhil.p.nagaraj@gmail.com>
2026-07-27 07:15:14 -0700

    patch old_function surgically

---
+++ src/hooks/patch_demo.py
@@ -0,0 +1,13 @@
+# Demo file for patch_function test
+
+def helper():
+    pass
+
+
+def old_function():
+    """Replacement function typed by Jimbibo."""
+  x = 10
+    y = 20
+    return x * y
+ another_helper():
+    pass
